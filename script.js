d8fc755911c7b8ea5af98b97f70e45fd0c114c91
vininRibeiro <vinin40@gmail.com>
2023-09-27 01:25:44 -0300

    complemento estudos js

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-const menu = document.querySelector('.menu');
-console.log(menu)
+const listaAnimais = document.querySelector('.animais-lista');
 
-const animais = document.querySelector('.animais');
-console.log(animais.attributes['data-texto'])
+const height = listaAnimais.scrollHeight;
+const animaisTop = listaAnimais.offsetTop;
+console.log(animaisTop)
 
-const img = document.querySelector('img');
-
-img.setAttribute('alt', 'è uma raposa ')
-
-console.log(img.getAttribute('src'))
-
-
-const intensMenu = document.querySelectorAll('.menu a')
-intensMenu.forEach((item) => {
-    item.classList.add('ativo')
-})
-intensMenu.forEach((item) => {
-    item.remove('ativo')
-})
-intensMenu[0].classList.add('ativo')
-
-//const itensRemove = document.querySelectorAll('.menu a')
-//itensRemove.forEach((item) =>{
-//    item.classList.remove('ativo')
-//})
-
-const imgs = document.querySelectorAll('img')
-
-imgs.forEach((img) =>{
-const possuiAtributo = img.hasAttribute('alt')
-console.log(img, possuiAtributo)
-})
-
-const link = document.querySelector('a[href^="http"]')
-link.setAttribute('href', 'https://www.google.com/');
-
-console.log(link)
-
+const primeiroh2 = document.querySelector('h2')
+const react = primeiroh2.getBoundingClientRect();
+console.log(react)
