c356ca2e2cb24e58701e552e84be68dcf922856f
vininRibeiro <vinin40@gmail.com>
2023-10-09 20:20:24 -0300

    dimensoes e distancias

--- script.js
+++ script.js
@@ -5,5 +5,28 @@ const animaisTop = listaAnimais.offsetTop;
 console.log(animaisTop)
 
 const primeiroh2 = document.querySelector('h2')
-const react = primeiroh2.getBoundingClientRect();
-console.log(react)
+const h2left = primeiroh2.offsetLeft;
+
+const h2react = primeiroh2.getBoundingClientRect();
+
+console.log(h2react.top);
+if(h2react.top)
+
+console.log(
+    window.innerWidth,
+    window.innerHeight,
+    window.outerWidth,
+    Window.outerHeight,
+    window.pageYOffset 
+    
+);
+
+
+
+const small = window.matchMedia('(max-width: 600px)').matches;
+console.log(small)
+if(small){
+    console.log('Usuario mobile ');
+}else{
+    console.log('Usuario desktop '); 
+}
