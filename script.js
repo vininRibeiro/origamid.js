f5b5ba20159863d1c3b6bb303c34b3a5597d70af
vininRibeiro <vinin40@gmail.com>
2024-01-24 17:48:35 -0300

    alteracoes

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-const listaAnimais = document.querySelector('.animais-lista');
+const  img = document.querySelector('img');
 
-const height = listaAnimais.scrollHeight;
-const animaisTop = listaAnimais.offsetTop;
-console.log(animaisTop)
+img.addEventListener('click', function(){
+    console.log('clicou')
+})
 
-const primeiroh2 = document.querySelector('h2')
-const h2left = primeiroh2.offsetLeft;
-
-const h2react = primeiroh2.getBoundingClientRect();
-
-console.log(h2react.top);
-if(h2react.top)
-
-console.log(
-    window.innerWidth,
-    window.innerHeight,
-    window.outerWidth,
-    Window.outerHeight,
-    window.pageYOffset 
-    
-);
-
-
-
-const small = window.matchMedia('(max-width: 600px)').matches;
-console.log(small)
-if(small){
-    console.log('Usuario mobile ');
-}else{
-    console.log('Usuario desktop '); 
-}
